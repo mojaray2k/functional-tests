af1454d513dc2cb68af011c14ab2a02fba555c95
Amen Ra <amenra@Amens-MacBook-Pro.local>
2020-08-07 09:02:11 -0400

    need tests for arrayInsert.test.ts cryptoUtils.test.ts and dateUtil.test.ts

---
+++ src/cryptoUtils.test.ts
@@ -0,0 +1,21 @@
+import {
+    AES,
+    enc,
+    mode,
+    pad,
+} from "crypto-js";
+
+const options = {
+    mode: mode.CBC,
+    padding: pad.Pkcs7
+};
+
+export function encrypt(encryptItem: string, secretKey: string) {
+    const bytes = AES.encrypt(encryptItem, secretKey, options);
+    return bytes.toString();
+}
+
+export function decrypt(decryptItem: string, secretKey: string) {
+    const bytes = AES.decrypt(decryptItem, secretKey, options);
+    return bytes.toString(enc.Utf8);
+}
