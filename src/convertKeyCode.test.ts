98414bf2e9e9b44a0c3ad49803689ecc522c0c5c
abdulsamadola <suleimanolamilekan03@gmail.com>
2020-08-15 10:00:58 +0100

    :white_check_mark: Updates Test

--- src/convertKeyCode.test.ts
+++ src/convertKeyCode.test.ts
@@ -1,9 +1,31 @@
 import convertKeyCode from "./convertKeyCode";
 
 describe("convertKeyCode", () => {
-  it("should return empty object", () => {
-    const event = new KeyboardEvent("");
-    const expected = {};
+  it("should return an object containing if the key is actually pressed", () => {
+    const event = new Object({ keyCode: 37 });
+
+    const expected = {
+      char: "%",
+      isCtrl: undefined,
+      isDir: true,
+      isDown: false,
+      isDownDir: false,
+      isEnd: false,
+      isEnter: false,
+      isEsc: false,
+      isHome: false,
+      isLeftDir: true,
+      isPageDown: false,
+      isPageUp: false,
+      isRightDir: false,
+      isShift: undefined,
+      isSpace: false,
+      isTab: false,
+      isUp: true,
+      isUpDir: false,
+      keyCode: 37,
+    };
+
     expect(convertKeyCode(event)).toEqual(expected);
   });
 });
--- src/cryptoUtils.test.ts
+++ src/cryptoUtils.test.ts
@@ -5,9 +5,13 @@ describe("encrypt and decrypt ", () => {
   const secretKey = "#passKey@321+%$i";
   const encryptedItem = encrypt(encryptItem, secretKey);
 
+  it("should return encrpted item", () => {
+    const expected = "U2FsdGVkX1";
+    expect(encrypt(encryptItem, secretKey)).toContain(expected);
+  });
+
   it("should return decrypted item", () => {
     const expected = "myPassword";
-
     expect(decrypt(encryptedItem, secretKey)).toBe(expected);
   });
 });
